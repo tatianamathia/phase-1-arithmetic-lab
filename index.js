0d347ab5df0c7c675d9695141cd46f3a7e975cda
Tatiana Mathia <mathiatatiana@gmail.com>
2024-03-19 14:09:44 +0300

    operates math in javascript

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random()*100)
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+
+let numbers = [10, 5, 20, 15]
+let max = Math.max(10, 5, 20, 15)
